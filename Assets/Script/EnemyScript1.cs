49e951c22b859b4d78cd3b111f3a49a32f59bbd1
AokiMutsuki <ca01254001@st.kawahara.ac.jp>
2025-08-06 10:40:02 +0900

    <ename>移動の追加 (#7)

---
+++ Assets/Script/EnemyScript1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript1 : MonoBehaviour
+{
+    Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posY = startPos.y + Mathf.Sin(Time.time) * 8;
+        transform.position = new
+        Vector3(transform.position.x, posY, transform.position.z);
+
+        
+    }
+}
